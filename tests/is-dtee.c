9072878b5c4dd77d9af9cef2a8e95912668a354d
Simon Arlott <sa.me.uk>
2018-09-30 15:49:47 +0100

    tests: Determine if the current process is dtee based on the executable filename
    There is no guarantee that the shared library constructor will be called with
    argc and argv as arguments.

--- tests/is-dtee.c
+++ tests/is-dtee.c
@@ -1,29 +1,19 @@
 #include "is-dtee.h"
 
 #include <libgen.h>
+#include <limits.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
-
-static int argc_copy = 0;
-static char **argv_copy = NULL;
-
-__attribute__((constructor)) static void dtee_test_is_dtee_main_copy(int argc, char **argv) {
-	argc_copy = argc;
-	argv_copy = argv;
-}
+#include <unistd.h>
 
 bool dtee_test_is_dtee(void) {
 	bool is_dtee = false;
+	char buf[PATH_MAX];
 
-	if (argc_copy > 0) {
-		char *argv0_copy = strdup(argv_copy[0]);
-		if (argv0_copy != NULL) {
-			const char *base_program_name = basename(argv0_copy);
+	if (readlink("/proc/self/exe", buf, sizeof(buf)) > 0) {
+		const char *base_program_name = basename(buf);
 
-			is_dtee = !strcmp(base_program_name, "dtee");
-			free(argv0_copy);
-		}
+		is_dtee = !strcmp(base_program_name, "dtee");
 	}
 
 	return is_dtee;
