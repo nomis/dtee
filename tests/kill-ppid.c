ab251e7a1bcdb3fe8104a2a74d9c85cdfd37da22
Simon Arlott <sa.me.uk>
2018-10-12 20:18:09 +0100

    tests: Kill tests need to allow enough time for the output to be received
    100ms should be enough time...

--- tests/kill-ppid.c
+++ tests/kill-ppid.c
@@ -2,10 +2,12 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
 	int signum;
+	struct timespec ts = { 0, 100 * 1000 * 1000 };
 
 	if (argc != 2) {
 		return EXIT_FAILURE;
@@ -15,6 +17,9 @@ int main(int argc, char *argv[]) {
 
 	printf("Killing parent process with signal %d\n", signum);
 	fflush(stdout);
+
+	// Allow enough time for the process to read our output before killing it
+	nanosleep(&ts, NULL);
 
 	kill(getppid(), signum);
 	return EXIT_SUCCESS;
