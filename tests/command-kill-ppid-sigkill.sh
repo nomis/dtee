2c71d1429347dd2ff95ace30c55adb1f5e4294f4
Simon Arlott <sa.me.uk>
2018-10-13 11:53:46 +0100

    tests: Check exit status was by signal when killed

--- tests/command-kill-ppid-sigkill.sh
+++ tests/command-kill-ppid-sigkill.sh
@@ -5,8 +5,16 @@ set -vx
 run_test_once ./test-kill-ppid 9
 RET=$?
 
+eval $(./test-waitpid ./dtee ./dtee ./test-kill-ppid 9)
+RET2=$?
+
 echo RET $RET
-if [ $RET -eq $((128 + 9)) ]; then
+echo RET2 $RET
+echo WIFEXITED $WIFEXITED
+echo WEXITSTATUS $WEXITSTATUS
+echo WIFSIGNALED $WIFSIGNALED
+echo WTERMSIG $WTERMSIG
+if [ $RET -eq $((128 + 9)) ] && [ $RET2 -eq 0 ] && [ $WIFSIGNALED -eq 1 ] && [ $WTERMSIG -eq 9 ]; then
 	exit 0
 fi
 exit 1
