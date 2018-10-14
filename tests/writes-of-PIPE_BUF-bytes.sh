324a956115bcf254abb0b527a3e1dec3b682b77e
Simon Arlott <sa.me.uk>
2018-10-14 13:27:18 +0100

    tests: Check writes of size 0 and PIPE_BUF bytes

---
+++ tests/writes-of-PIPE_BUF-bytes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -vx
+. "$(dirname "$0")"/util/common.sh
+
+function test_prepare() {
+	rm -f "$TESTDIR/$NAME.file.out-overwrite.txt"
+}
+
+run_test -O "$TESTDIR/$NAME.file.out-overwrite.txt" ./test-writes-of-n-bytes PIPE_BUF 0
+RET=$?
+
+cmp "$TESTDIR/$NAME.file.out-overwrite.txt" "${0/.sh/.file.out-overwrite.txt}"
+CMP_OUT_O=$?
+[ $CMP_OUT_O -ne 0 ] && diff -U4 "${0/.sh/.file.out-overwrite.txt}" "$TESTDIR/$NAME.file.out-overwrite.txt"
+
+echo RET $RET
+echo CMP_OUT_O $CMP_OUT_O
+if [ $RET -eq 0 ] && [ $CMP_OUT_O -eq 0 ]; then
+	exit 0
+fi
+exit 1
