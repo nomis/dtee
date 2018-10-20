5d1f4826eb365230626da9dfc1a07ea95ae28dc0
Simon Arlott <sa.me.uk>
2018-10-20 11:30:55 +0100

    input: Destructor does not need to be virtual

--- src/input.h
+++ src/input.h
@@ -37,7 +37,7 @@ namespace dtee {
 class Input {
 public:
 	explicit Input(std::shared_ptr<Output> output);
-	virtual ~Input();
+	~Input();
 
 	bool open(bool handle_interrupt_signals);
 	void fork_prepare();
