5f4b99f399e53d363e275300fb4763db2ffa0109
Simon Arlott <sa.me.uk>
2018-10-28 15:45:13 +0000

    tests: Check writes of size 1 byte to stdout and stderr

---
+++ tests/writes-of-1-byte-stdout.sh
@@ -0,0 +1,14 @@
+. "$(dirname "$0")"/util/common.sh
+
+function test_prepare() {
+	rm -f "$TESTDIR/$NAME.file.out-overwrite.txt"
+}
+
+run_test -O "$TESTDIR/$NAME.file.out-overwrite.txt" ./test-writes-of-n-bytes STDOUT_FILENO 1 0
+RET=$?
+
+cmp_files "file.out-overwrite"
+CMP_OUT_O=$?
+
+variables_must_eq RET $EXIT_SUCCESS \
+	CMP_OUT_O 0
