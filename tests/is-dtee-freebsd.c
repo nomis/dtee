df314b280fde516def615f51368a3e83ea110f83
Simon Arlott <sa.me.uk>
2018-10-28 14:31:47 +0000

    tests: simplify parts of is-dtee-freebsd

--- tests/is-dtee-freebsd.c
+++ tests/is-dtee-freebsd.c
@@ -9,13 +9,15 @@
 #include <fcntl.h>
 #include <kvm.h>
 #include <libgen.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 
-static bool dtee_test_is_proc_dtee(pid_t pid) {
-	bool is_dtee = false;
+static const char *dtee_test_read_proc_basename(pid_t pid, char *buf, size_t buflen) {
+	memset(buf, 0, buflen);
+
 	kvm_t *kd;
 	int cnt = 0;
 	struct kinfo_proc *proc;
@@ -24,14 +26,17 @@ static bool dtee_test_is_proc_dtee(pid_t pid) {
 	if (kd != NULL) {
 		proc = kvm_getprocs(kd, KERN_PROC_PID, pid, &cnt);
 		if (proc != NULL && cnt > 0) {
-			const char *base_program_name = basename(proc[0].ki_comm);
-
-			is_dtee = !strcmp(base_program_name, "dtee");
+			strncpy(buf, basename(proc[0].ki_comm), buflen - 1);
 		}
 		kvm_close(kd);
 	}
 
-	return is_dtee;
+	return buf;
+}
+
+static pid_t dtee_test_real_getpid(void) {
+	// Don't call getpid() because it may be fake
+	return syscall(SYS_getpid);
 }
 
 bool dtee_test_is_dtee(void) {
@@ -39,12 +44,10 @@ bool dtee_test_is_dtee(void) {
 	static __thread bool active = false;
 
 	if (!active) {
+		char buf[PATH_MAX + 1];
+
 		active = true;
-
-		// Don't call getpid() because it may be fake
-		pid_t pid = syscall(SYS_getpid);
-		is_dtee = dtee_test_is_proc_dtee(pid);
-
+		is_dtee = !strcmp(dtee_test_read_proc_basename(dtee_test_real_getpid(), buf, sizeof(buf)), "dtee");
 		active = false;
 	}
 
@@ -56,34 +59,14 @@ bool dtee_test_is_ppid_dtee(void) {
 	static __thread bool active = false;
 
 	if (!active) {
+		char buf[PATH_MAX + 1];
+
 		active = true;
-
-		is_ppid_dtee = dtee_test_is_proc_dtee(getppid());
-
+		is_ppid_dtee = !strcmp(dtee_test_read_proc_basename(getppid(), buf, sizeof(buf)), "dtee");
 		active = false;
 	}
 
 	return is_ppid_dtee;
-}
-
-static bool dtee_test_is_proc_dtee_test(pid_t pid) {
-	bool is_dtee_test = false;
-	kvm_t *kd;
-	int cnt = 0;
-	struct kinfo_proc *proc;
-
-	kd = kvm_open(NULL, "/dev/null", NULL, O_RDONLY, NULL);
-	if (kd != NULL) {
-		proc = kvm_getprocs(kd, KERN_PROC_PID, pid, &cnt);
-		if (proc != NULL && cnt > 0) {
-			const char *base_program_name = basename(proc[0].ki_comm);
-
-			is_dtee_test = !strncmp(base_program_name, "dtee-", 5);
-		}
-		kvm_close(kd);
-	}
-
-	return is_dtee_test;
 }
 
 bool dtee_test_is_dtee_test(void) {
@@ -91,12 +74,10 @@ bool dtee_test_is_dtee_test(void) {
 	static __thread bool active = false;
 
 	if (!active) {
+		char buf[PATH_MAX + 1];
+
 		active = true;
-
-		// Don't call getpid() because it may be fake
-		pid_t pid = syscall(SYS_getpid);
-		is_dtee_test = dtee_test_is_proc_dtee_test(pid);
-
+		is_dtee_test = !strncmp(dtee_test_read_proc_basename(dtee_test_real_getpid(), buf, sizeof(buf)), "dtee-", 5);
 		active = false;
 	}
 
