0ea23144c0953c12fdd1454b62a731f3ebcf792d
Simon Arlott <sa.me.uk>
2018-10-28 14:54:21 +0000

    tests: Add missing declaration of __dtee_test_getppid()

--- tests/is-dtee.h
+++ tests/is-dtee.h
@@ -1,12 +1,14 @@
 #ifndef DTEE_IS_DTEE_H_
 #define DTEE_IS_DTEE_H_
 
+#include <sys/types.h>
 #include <stdbool.h>
 
 bool dtee_test_is_dtee(void);
 bool dtee_test_is_ppid_dtee(void);
 bool dtee_test_is_dtee_test(void);
 
+pid_t __dtee_test_getppid(void);
 bool __dtee_test_is_dtee(const char *exe_name);
 bool __dtee_test_is_dtee_test(const char *exe_name);
 
