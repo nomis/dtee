5d1ff061b38549a52fffb58240a4f9769eccc964
Simon Arlott <sa.me.uk>
2018-10-31 12:32:42 +0000

    tests: Merge lseek and lseek64 library

--- tests/cron-file-seek-failure-cron.sh
+++ tests/cron-file-seek-failure-cron.sh
@@ -1,7 +1,7 @@
 . "$(dirname "$0")"/util/common.sh
 
 # If the temporary output file cannot be read, it should output an error message
-TEST_LD_PRELOAD="./libtest-mkostemp-consistent.so:./libtest-lseek-failure.so:./libtest-lseek64-failure.so:./libtest-fake-getpid.so:./libtest-fake-getuid.so"
+TEST_LD_PRELOAD="./libtest-mkostemp-consistent.so:./libtest-lseek-failure.so:./libtest-fake-getpid.so:./libtest-fake-getuid.so"
 run_test "-q" "$RUN"
 RET=$?
 
--- tests/lseek-failure.c
+++ tests/lseek-failure.c
@@ -1,7 +1,13 @@
+#define lseek64 __dtee__hide__lseek64__
+#define lseek __dtee__hide__lseek__
+
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <errno.h>
 #include <unistd.h>
+
+#undef lseek
+#undef lseek64
 
 #include "is-dtee.h"
 
@@ -25,4 +31,26 @@ off_t lseek(int fd, off_t offset, int whence) {
 	}
 
 	return (*next_lseek)(fd, offset, whence);
+}
+
+static off64_t dtee_test_lseek64_failure(int fd __attribute__((unused)), off64_t offset __attribute__((unused)), int whence __attribute__((unused))) {
+	errno = EINVAL;
+	return -1;
+}
+
+off64_t lseek64(int fd, off64_t offset, int whence) {
+	off64_t (*next_lseek64)(int, off64_t, int) = dlsym(RTLD_NEXT, "lseek64");
+	static __thread bool active = false;
+
+	if (!active) {
+		active = true;
+
+		if (dtee_test_is_dtee()) {
+			next_lseek64 = dtee_test_lseek64_failure;
+		}
+
+		active = false;
+	}
+
+	return (*next_lseek64)(fd, offset, whence);
 }
