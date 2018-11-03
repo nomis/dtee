9eb2f11288c91260429d440d1e9dca48300c9fa8
Simon Arlott <sa.me.uk>
2018-11-03 17:36:27 +0000

    tests: Implement __socket30 on NetBSD and avoid rename of socket

--- tests/socket-unix-failure.c
+++ tests/socket-unix-failure.c
@@ -1,9 +1,15 @@
+#define __socket30 __dtee__hide___socket30
+#define socket __dtee__hide__socket
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+#undef socket
+#undef __socket30
 
 #include "allow-n-times.h"
 #include "is-dtee.h"
@@ -35,4 +41,28 @@ int socket(int domain, int type, int protocol) {
 	}
 
 	return (*next_socket)(domain, type, protocol);
+}
+
+int __socket30(int domain, int type, int protocol) {
+	int (*next___socket30)(int, int, int) = dlsym(RTLD_NEXT, "__socket30");
+	static __thread bool active = false;
+
+	if (!active) {
+		active = true;
+
+		if (dtee_test_is_dtee()) {
+			if (domain == AF_UNIX) {
+				static unsigned long allowed = 0;
+				static bool first = true;
+
+				if (!dtee_test_allow_n_times("DTEE_TEST_SOCKET_UNIX_FAILURE_ALLOW", &first, &allowed)) {
+					next___socket30 = dtee_test_socket_failure;
+				}
+			}
+		}
+
+		active = false;
+	}
+
+	return (*next___socket30)(domain, type, protocol);
 }
