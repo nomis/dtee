49c35f14f3bbb2f144e7aed720247a335ec69fe9
Simon Arlott <sa.me.uk>
2018-12-02 12:16:45 +0000

    tests: Windows Subsystem for Linux (4.4.0-43) always claims to have written a core dump on SIGQUIT

--- tests/cron-command-sigquit-no-coredump.sh
+++ tests/cron-command-sigquit-no-coredump.sh
@@ -1,5 +1,8 @@
 . "$(dirname "$0")"/util/common.sh
 
+# Windows Subsystem for Linux (4.4.0-43) always claims to have written a core dump on SIGQUIT
+[[ $(uname -r) =~ Microsoft ]] && exit $TEST_EX_SKIP
+
 # Increase core size to unlimited and then the command will disable core dumps
 ulimit -H -c unlimited || exit $TEST_EX_SKIP
 ulimit -S -c unlimited || exit $TEST_EX_SKIP
