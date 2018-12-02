0a738050b364c642707c04a4cc3e46f5405c104c
Simon Arlott <sa.me.uk>
2018-12-02 10:31:00 +0000

    tests: Allow for additional recv() calls on Cygwin

--- tests/cron-input-socket-recv-failure.sh
+++ tests/cron-input-socket-recv-failure.sh
@@ -1,7 +1,16 @@
 . "$(dirname "$0")"/util/common.sh
 
 # Allow the first few lines to work
-export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=3
+case "$UNAME" in
+	CYGWIN_*)
+		# There are two extra recv calls during setup of the output sockets
+		export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=5
+		;;
+
+	*)
+		export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=3
+		;;
+esac
 
 TEST_LD_PRELOAD=(test-socket-recv-failure)
 run_test -q ./test-lorem-ipsum-late-exit
--- tests/input-socket-recv-failure.sh
+++ tests/input-socket-recv-failure.sh
@@ -1,7 +1,16 @@
 . "$(dirname "$0")"/util/common.sh
 
 # Allow the first few lines to work
-export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=4
+case "$UNAME" in
+	CYGWIN_*)
+		# There are two extra recv calls during setup of the output sockets
+		export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=6
+		;;
+
+	*)
+		export DTEE_TEST_SOCKET_RECV_FAILURE_ALLOW=4
+		;;
+esac
 
 TEST_LD_PRELOAD=(test-socket-recv-failure)
 run_test "$RUN"
