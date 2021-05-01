e0ea28f2c3159cea807fd4c3fffe443e16ea8d26
Simon Arlott <sa.me.uk>
2021-05-01 12:31:26 +0100

    tests: Override libintl_bindtextdomain as well as bindtextdomain

--- tests/bindtextdomain-override.c
+++ tests/bindtextdomain-override.c
@@ -1,7 +1,13 @@
+#define libintl_bindtextdomain __dtee__hide__libintl_bindtextdomain
+#define bindtextdomain __dtee__hide__bindtextdomain
+
 #include <dlfcn.h>
 #include <libintl.h>
 #include <stdbool.h>
 #include <stddef.h>
+
+#undef bindtextdomain
+#undef libintl_bindtextdomain
 
 #include "is-dtee.h"
 #include "dtee-fcn.h"
@@ -28,4 +34,28 @@ TEST_FCN_REPL(char*, bindtextdomain, (const char *domainname, const char *dirnam
 	}
 
 	return (*next_bindtextdomain)(domainname, dirname);
+}
+
+TEST_FCN_DECL(char*, libintl_bindtextdomain, (const char *domainname, const char *dirname));
+
+static char * dtee_test_override_libintl_bindtextdomain(const char *domainname, const char *dirname __attribute__((unused))) {
+	char* (*next_libintl_bindtextdomain)(const char *, const char *) = TEST_FCN_NEXT(libintl_bindtextdomain);
+	return (*next_libintl_bindtextdomain)(domainname, GETTEXT_LOCALEDIR);
+}
+
+TEST_FCN_REPL(char*, libintl_bindtextdomain, (const char *domainname, const char *dirname)) {
+	char* (*next_libintl_bindtextdomain)(const char *, const char *) = TEST_FCN_NEXT(libintl_bindtextdomain);
+	static __thread bool active = false;
+
+	if (!active) {
+		active = true;
+
+		if (dtee_test_is_dtee()) {
+			next_libintl_bindtextdomain = dtee_test_override_libintl_bindtextdomain;
+		}
+
+		active = false;
+	}
+
+	return (*next_libintl_bindtextdomain)(domainname, dirname);
 }
