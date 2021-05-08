80cb3f27c66cb610ab6eec19fc33ef6a7830631a
Simon Arlott <sa.me.uk>
2021-05-08 09:16:07 +0100

    tests: Improve comment

--- tests/basic/cron-throw-exception.sh
+++ tests/basic/cron-throw-exception.sh
@@ -1,7 +1,7 @@
 . "$(dirname "$0")"/../util/common.sh
 
 TEST_LD_PRELOAD=(test-write-throw-exception)
-# If there's an exception then dtee will exit
+# If there's an exception then dtee will flush buffered output and exit
 run_test -q "$RUN"
 RET=$?
 
