0c8db5261fbb1d815584b547d01d9606a9099f59
Simon Arlott <sa.me.uk>
2021-05-09 15:51:40 +0100

    tests: Avoid leaving around test files that are not easily removable

--- tests/input/temp-dir-permission-denied.sh
+++ tests/input/temp-dir-permission-denied.sh
@@ -2,7 +2,14 @@
 
 is_acl_directory_traversal_override && exit $TEST_EX_SKIP
 
+function cleanup() {
+	chmod u+rwx "$ORIG_TMPDIR/permission-denied"
+}
+
+ORIG_TMPDIR="$TMPDIR"
+
 mkdir -p "$TMPDIR/permission-denied/tmp"
+trap cleanup EXIT
 chmod 000 "$TMPDIR/permission-denied"
 
 export TMPDIR="$TMPDIR/permission-denied/tmp"
