4606691629c1fb5d7b6200aa44b725387bb4a02d
Simon Arlott <sa.me.uk>
2021-11-15 22:42:30 +0000

    tests: Unblock signals
    Something is now blocking SIGQUIT while the tests are running in Jenkins

--- tests/util/default-signal-handlers.c
+++ tests/util/default-signal-handlers.c
@@ -1,10 +1,19 @@
+#include <assert.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sysexits.h>
 #include <unistd.h>
 
+static bool restore_signal(sigset_t *set, int signum) {
+	sigaddset(set, signum);
+	return signal(signum, SIG_DFL) != SIG_ERR;
+}
+
 int main(int argc, char *argv[]) {
+	sigset_t set;
+
 	if (argc < 2) {
 		printf("%s COMMAND [ARG]...\n", argv[0]);
 		return EX_USAGE;
@@ -16,16 +25,26 @@ int main(int argc, char *argv[]) {
 	// running the command otherwise tests involving SIGINT and SIGQUIT will not
 	// behave as expected. Reset other signal handlers too just in case
 	// something else has ignored them.
-	signal(SIGHUP, SIG_DFL);
-	signal(SIGINT, SIG_DFL);
-	signal(SIGQUIT, SIG_DFL);
-	signal(SIGABRT, SIG_DFL);
-	signal(SIGSEGV, SIG_DFL);
-	signal(SIGPIPE, SIG_DFL);
-	signal(SIGALRM, SIG_DFL);
-	signal(SIGTERM, SIG_DFL);
-	signal(SIGCHLD, SIG_DFL);
-	signal(SIGCONT, SIG_DFL);
+
+	sigemptyset(&set);
+
+	assert(restore_signal(&set, SIGHUP));
+	assert(restore_signal(&set, SIGINT));
+	assert(restore_signal(&set, SIGQUIT));
+	assert(restore_signal(&set, SIGABRT));
+	assert(restore_signal(&set, SIGSEGV));
+	assert(restore_signal(&set, SIGPIPE));
+	assert(restore_signal(&set, SIGALRM));
+	assert(restore_signal(&set, SIGTERM));
+	assert(restore_signal(&set, SIGCHLD));
+	assert(restore_signal(&set, SIGCONT));
+
+	// Also unblock the signals!
+
+	if (sigprocmask(SIG_UNBLOCK, &set, NULL) != 0) {
+		perror("sigprocmask");
+		return EX_OSERR;
+	}
 
 	execvp(argv[1], &argv[1]);
 	perror("execvp");
