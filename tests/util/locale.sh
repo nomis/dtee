6a53620974e3d635765dc9fae8ff84f02e55af97
Simon Arlott <sa.me.uk>
2024-04-07 16:40:24 +0100

    tests: Prefer test language locale to be UTF-8

--- tests/util/locale.sh
+++ tests/util/locale.sh
@@ -3,26 +3,34 @@ mkdir -p "util/$BASETESTDIR/locale/$TEST_LANGUAGE/LC_MESSAGES"
 ln -s "../../../../../../i18n/test-${TEST_LANGUAGE}.mo" "util/$BASETESTDIR/locale/$TEST_LANGUAGE/LC_MESSAGES/dtee.mo"
 
 # Find the best possible system language to use when running the test
-FOUND_LOCALES=("" "" "" "")
+FOUND_LOCALES=("" "" "" "" "" "")
 while read -r line; do
 	case "$line" in
 		C|POSIX)
 			;;
 
 		C.utf8|C.UTF8|C.utf-8|C.UTF-8)
-			FOUND_LOCALES[3]="$line"
+			FOUND_LOCALES[5]="$line"
 			;;
 
-		"$TEST_LANGUAGE"|"$TEST_LANGUAGE".*)
+		"$TEST_LANGUAGE".utf8|"$TEST_LANGUAGE".UTF8|"$TEST_LANGUAGE".utf-8|"$TEST_LANGUAGE".UTF-8)
 			FOUND_LOCALES[0]="$line"
 			;;
 
-		*.utf8|*.UTF8|*.utf-8|*.UTF-8)
+		"$TEST_LANGUAGE".*)
 			FOUND_LOCALES[1]="$line"
 			;;
 
-		*)
+		"$TEST_LANGUAGE")
 			FOUND_LOCALES[2]="$line"
+			;;
+
+		*.utf8|*.UTF8|*.utf-8|*.UTF-8)
+			FOUND_LOCALES[3]="$line"
+			;;
+
+		*)
+			FOUND_LOCALES[4]="$line"
 			;;
 	esac
 done < <(locale -a)
