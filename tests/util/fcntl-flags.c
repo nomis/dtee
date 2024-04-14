3cbb7962b3f07749c641a0517332711128d43cc1
Simon Arlott <sa.me.uk>
2024-04-14 15:07:02 +0100

    tests: Avoid using __builtin_apply() by implementing most fcntl() commands

--- tests/util/fcntl-flags.c
+++ tests/util/fcntl-flags.c
@@ -1,6 +1,8 @@
+#include <assert.h>
 #include <dlfcn.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,6 +58,7 @@ static int dtee_test_fcntl_failure(int fd __attribute__((unused)), int cmd __att
 }
 
 static int dtee_test_fcntl_return_non_blocking(int fd, int cmd, ...) {
+	assert(cmd == F_GETFL);
 	int (*next_fcntl)(int, int, ...) = TEST_FCN_NEXT(fcntl);
 	int ret = next_fcntl(fd, cmd);
 
@@ -67,7 +70,7 @@ static int dtee_test_fcntl_return_non_blocking(int fd, int cmd, ...) {
 }
 
 TEST_FCN_REPL(int, fcntl, (int fd, int cmd, ...)) {
-	void *args = __builtin_apply_args();
+	va_list ap;
 	int (*next_fcntl)(int, int, ...) = TEST_FCN_NEXT(fcntl);
 	static __thread bool active = false;
 
@@ -85,11 +88,101 @@ TEST_FCN_REPL(int, fcntl, (int fd, int cmd, ...)) {
 		active = false;
 	}
 
-#pragma GCC diagnostic push
-#if __GNUC__ >= 8
-# pragma GCC diagnostic ignored "-Wcast-function-type"
+	va_start(ap, cmd);
+	int ret;
+
+	switch (cmd) {
+		// (void)
+	case F_GETFD:
+	case F_GETFL:
+#ifdef F_GETOWN
+	case F_GETOWN:
 #endif
-	void *ret = __builtin_apply((void (*)())next_fcntl, args, 1024);
-#pragma GCC diagnostic pop
-	__builtin_return(ret);
+#ifdef F_GETSIG
+	case F_GETSIG:
+#endif
+#ifdef F_GETLEASE
+	case F_GETLEASE:
+#endif
+#ifdef F_GETPIPE_SZ
+	case F_GETPIPE_SZ:
+#endif
+#ifdef F_GET_SEALS
+	case F_GET_SEALS:
+#endif
+		ret = next_fcntl(fd, cmd);
+		break;
+
+		// (int)
+	case F_DUPFD:
+#ifdef F_DUPFD_CLOEXEC
+	case F_DUPFD_CLOEXEC:
+#endif
+	case F_SETFD:
+	case F_SETFL:
+#ifdef F_SETOWN
+	case F_SETOWN:
+#endif
+#ifdef F_SETSIG
+	case F_SETSIG:
+#endif
+#ifdef F_SETLEASE
+	case F_SETLEASE:
+#endif
+#ifdef F_NOTIFY
+	case F_NOTIFY:
+#endif
+#ifdef F_SETPIPE_SZ
+	case F_SETPIPE_SZ:
+#endif
+#ifdef F_ADD_SEALS
+	case F_ADD_SEALS:
+#endif
+		ret = next_fcntl(fd, cmd, va_arg(ap, int));
+		break;
+
+		// (void*)
+	case F_SETLK:
+	case F_SETLKW:
+	case F_GETLK:
+#ifdef F_OFD_SETLK
+	case F_OFD_SETLK:
+#endif
+#ifdef F_OFD_SETLKW
+	case F_OFD_SETLKW:
+#endif
+#ifdef F_OFD_GETLK
+	case F_OFD_GETLK:
+#endif
+#ifdef F_GETOWN_EX
+	case F_GETOWN_EX:
+#endif
+#ifdef F_SETOWN_EX
+	case F_SETOWN_EX:
+#endif
+#ifdef F_GET_RW_HINT
+	case F_GET_RW_HINT:
+#endif
+#ifdef F_SET_RW_HINT
+	case F_SET_RW_HINT:
+#endif
+#ifdef F_GET_FILE_RW_HINT
+	case F_GET_FILE_RW_HINT:
+#endif
+#ifdef F_SET_FILE_RW_HINT
+	case F_SET_FILE_RW_HINT:
+#endif
+		ret = next_fcntl(fd, cmd, va_arg(ap, void *));
+		break;
+
+	default:
+		fprintf(stderr, "unhandled fcntl(fd=%d, cmd=%d, ...)\n", fd, cmd);
+		fflush(stderr);
+		abort();
+	}
+
+	int errno_copy = errno;
+	va_end(ap);
+	errno = errno_copy;
+	return ret;
 }
